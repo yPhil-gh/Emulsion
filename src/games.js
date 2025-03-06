b648373bc6102e4dbad4a51dec07f45e267e9cab
yassinphilip <xaccrocheur@gmail.com>
2025-03-06 21:45:53 +0000

    Reset working directory to empty

--- src/games.js
+++
@@ -1,7 +0,0 @@
-window.electronAPI.onPopulateGames((event, data) => {
-  console.log('Games Data:', data);
-  document.getElementById('user-data-path').textContent = data.userDataPath;
-
-  // Confirm that the page is ready
-  window.electronAPI.sendPageReady();
-});
--- src/settings.js
+++
@@ -1,7 +0,0 @@
-window.electronAPI.onPopulateSettings((event, data) => {
-  console.log('Settings Data:', data);
-  document.getElementById('user-data-path').textContent = data.userDataPath;
-
-  // Confirm that the page is ready
-  window.electronAPI.sendPageReady();
-});

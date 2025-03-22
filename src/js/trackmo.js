aa4ecf2fc40b7faa8f7749a8eea078f64402f673
yassinphilip <xaccrocheur@gmail.com>
2025-03-22 04:09:47 +0000

    Double waves

--- src/js/trackmo.js
+++ src/js/trackmo.js
@@ -1,26 +1,29 @@
-const container = document.querySelector('.circle');
+const container = document.querySelector('.liquid');
 
 function hslColors () {
-  let colors = [];
-  let hue = Math.floor(Math.random() * (320 - 153) + 153);
-  let saturation = Math.floor(Math.random() * (100 - 90) + 90);
-  let lightness = Math.floor(Math.random() * (80 - 70) + 70);
+    let bubbles = [];
+    let hue = Math.floor(Math.random() * (320 - 153) + 153);
+    let saturation = Math.floor(Math.random() * (100 - 90) + 90);
+    let lightness = Math.floor(Math.random() * (80 - 70) + 70);
 
-  colors.push(`hsl(${hue},${saturation}%, ${lightness}%)`);
-  return colors;
+    bubbles.push(`hsl(${hue},${saturation}%, ${lightness}%)`);
+    return bubbles;
 };
 
+function bubblePos() {
+    return Math.floor(Math.random() * 19) + 177;
+}
 
-function colors() {
+function bubbles() {
   let template = `
-  <div class="color" style="background: ${hslColors()};">       </div>
+  <div class="bubble" style="background: ${hslColors()}; left: ${bubblePos()}"></div>
   `;
 
   container.insertAdjacentHTML('beforeend', template);
 };
 
 for(i = 0; i < 10; i++) {
-  colors();
+  bubbles();
 };
 
 // Vars -----------------------------
